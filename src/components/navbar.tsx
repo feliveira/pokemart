bdfe2de8aaa343f3254dc97caf3574641313737f
Felipe Oliveira <feliveira8@gmail.com>
2023-10-23 07:12:57 -0300

    feat: creating navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,14 @@
+// import CartButton from "./cart-button"
+
+function Navbar( ) {
+  return (
+    <nav className="bg-white w-full px-8 lg:px-40 py-4  sticky top-0 z-50 shadow-xl">
+        <div className="w-full max-w-[1200px] flex items-center justify-between mx-auto">
+            <p className="text-[#E6B13D] font-extrabold text-2xl">Pokemart</p>
+            {/* <CartButton /> */}
+        </div>
+    </nav>
+    )
+}
+
+export default Navbar
